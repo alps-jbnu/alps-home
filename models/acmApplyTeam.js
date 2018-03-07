b36030383f7343aac67012f59bf55be9a0d9730f
ALPS server <joonas.yoon@gmail.com>
2018-03-07 15:18:33 +0900

    Add Phone numbers

--- models/acmApplyTeam.js
+++ models/acmApplyTeam.js
@@ -6,10 +6,13 @@ var acmApplyTeamSchema = new Schema({
     phone: String,
     member1_name: String,
     member1_sid: String,
+    member1_mail: String,
     member2_name: String,
     member2_sid: String,
+    member2_mail: String,
     member3_name: String,
     member3_sid: String,
+    member3_mail: String,
     registered_at: {
         type: Date,
         default: Date.now
